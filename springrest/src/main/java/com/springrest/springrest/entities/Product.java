b89de2160c896650b4e0c5b1b9cfd15717206810
KhushalMitbaokar <55062096+KhushalMitbaokar@users.noreply.github.com>
2022-04-01 16:30:20 +0530

    Add files via upload

--- springrest/src/main/java/com/springrest/springrest/entities/Product.java
+++ springrest/src/main/java/com/springrest/springrest/entities/Product.java
@@ -1,10 +1,10 @@
 package com.springrest.springrest.entities;
 
 public class Product {
-	private long id;
+	private int id;
 	private String Name;
 	private String Discription;
-	public Product(long id, String name, String discription) {
+	public Product(int id, String name, String discription) {
 		super();
 		this.id = id;
 		Name = name;
@@ -14,10 +14,10 @@ public class Product {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	public long getId() {
+	public int getId() {
 		return id;
 	}
-	public void setId(long id) {
+	public void setId(int id) {
 		this.id = id;
 	}
 	public String getName() {
